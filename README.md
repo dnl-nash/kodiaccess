98a584813d14fa474f9d8594e3f8b286436a3d69
Daniel Nash <dnl.nash@gmail.com>
2023-06-10 02:43:13 -0400

    Initial commit to version control.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # kodiaccess
-Installs the Kodi screen reader, providing cross platform non-visual access to many different types of media
+# kodiaccess
+Kodi Screen Reader Automatic Installer
+This script for windows will install the Kodi Media Center, along with the Kodi Screen Reader, allowing eyes-free access to the platform and its addons. The script will do the following:
+    Check for and install both the SQLite3 DBMS, along with Git for Windows tools, which are needed to configure accessibility for kodi
+    Check if the Kodi Media Center program is available in the default location. If it is not, it will be downloaded and installed on both windows and Mac OS X.
+    Create the default structure for Kodi to detect and install addons
+    Install and configure the screen reader
+    Run kodi to populate the initial user profile
+    Set all addons to enabled state
+This script will also install accessibility for the flatpak version of kodi, which will be installed automatically if it has not already been installed by your system's package manager.
---
+++ kodiaccess.bat
@@ -0,0 +1,34 @@
+set mydir=%cd%
+set sqlite3=%mydir%\kodiaccessutils\sqlite\sqlite-tools-win32-x86-3420000\sqlite3.exe
+set git=%mydir%\kodiaccessutils\git\cmd\git.exe
+if not exist %sqlite3% goto depinstall
+:depinstall
+mkdir %mydir%\kodiaccessutils
+cd %mydir%\kodiaccessutils
+curl -Lo sqlite.zip https://sqlite.org/2023/sqlite-tools-win32-x86-3420000.zip
+curl -Lo git.zip "https://github.com/git-for-windows/git/releases/download/v2.41.0.windows.1/MinGit-2.41.0-64-bit.zip"
+powershell expand-archive sqlite.zip
+powershell expand-archive git.zip
+goto main
+:main
+if exist "c:\Program Files\Kodi\kodi.exe" goto :ttsinstall else goto :dlkodi
+:dlkodi
+curl -Lo kodiinst.exe https://mirrors.kodi.tv/releases/windows/win64/kodi-20.1-Nexus-x64.exe
+kodiinst.exe /S
+del kodiinst.exe
+goto ttsinstall
+:ttsinstall
+mkdir %appdata%\kodi\addons %appdata%\kodi\userdata\addon_data\service.xbmc.tts %appdata%\kodi\userdata\keymaps
+cd %appdata%\kodi\addons
+%git% clone --recursive https://github.com/dnl-nash/service.xbmc.tts -b 2to3
+cd %appdata%\kodi
+copy addons\service.xbmc.tts\resources\settings.xml userdata\addon_data\service.xbmc.tts
+copy addons\service.xbmc.tts\resources\keymap.base.xml userdata\keymaps\service.xbmc.tts.keyboard.xml
+cd %userprofile%
+start "" "c:\Program Files\Kodi\kodi.exe"
+ping -n 10 localhost > NUL
+taskkill /f /im kodi.exe
+%sqlite3% %appdata%\kodi\userdata\Database\Addons33.db "update installed set enabled=1 where enabled=0;"
+rd /s /q %mydir%\kodiaccessutils
+exit
+goto main
